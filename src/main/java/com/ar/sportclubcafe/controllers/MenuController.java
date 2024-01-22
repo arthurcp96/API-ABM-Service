24158baf14bbb8d7556e72183148d9efd8a360a8
arthurcp96 <arthurcp96@gmail.com>
2024-01-22 16:57:12 -0300

    CrossOrigin en controller

--- src/main/java/com/ar/sportclubcafe/controllers/MenuController.java
+++ src/main/java/com/ar/sportclubcafe/controllers/MenuController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +28,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1")
-
+@CrossOrigin(origins = "http://localhost:4200")
 public class MenuController {
 
     @Autowired
