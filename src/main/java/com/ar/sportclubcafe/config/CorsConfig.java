721253af61feb1e2b027fc67a68f0e9d99c2f6ac
Arthur Cañari <84679797+arthurcp96@users.noreply.github.com>
2026-03-03 23:55:29 -0300

    fix cors para permitir consumo de api

--- src/main/java/com/ar/sportclubcafe/config/CorsConfig.java
+++ src/main/java/com/ar/sportclubcafe/config/CorsConfig.java
@@ -17,9 +17,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("allowedOrigin")
+                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET","POST","DELETE","PUT")
                         .allowedHeaders("*");
+                        
             }
             
         };
